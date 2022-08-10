99df79fcbb59b63de17188941f42f363991362dc
DeAndre Hai Jie Lim <deandrehaijiel@gmail.com>
2022-08-10 23:43:21 +0800

    On branch master Your branch is up to date with 'origin/master'.
    Changes to be committed:
    	modified:   package-lock.json
    	modified:   package.json
    	modified:   public/favicon.ico
    	modified:   src/App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.scss'
+import {Helmet} from "react-helmet";
 import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Home from './components/Home'
@@ -10,6 +11,12 @@ import Project from './components/Project'
 function App() {
   return (
     <>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>DeAndre Hai Jie L</title>
+      <link rel="canonical" href="http://mysite.com/example" />
+      <meta name="description" content="DeAndre Hai Jie Lim's Portfolio Website" />
+    </Helmet>
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
